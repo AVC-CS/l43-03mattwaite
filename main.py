cc5785484d74585ee5735161d6bfbcffe8f3340d
03mattwaite <mwaite2@students.avc.edu>
2025-04-10 15:02:39 -0700

    L43 ATT1

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ def main():
     ########################################
     """
     total = 0
-
+    count = 0 
+    
+    while count < 5:
+        number = int(input(f"Enter a number{count + 1}:"))
+        total += number 
+        count += 1
+        
+    print("Total:",total)
+        
     ########################################
     # Do not delete the return statement
     ########################################
